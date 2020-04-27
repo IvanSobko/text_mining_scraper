79f2c57050e1f9ad62742471fc7b5fe05fdb1d3b
Ivan Sobko <sobko.ivan@gmail.com>
2020-04-27 23:02:06 +0300

    added utf8 option to writing file

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def generate_text(url, iteration):
     soup = BeautifulSoup(response.text, "html.parser")
     similar_articles = soup.find_all("a", {"class": "similar"}, href=True)
     for l in similar_articles:
-        if l not in links_list:
-            links_list.append(f"https://cyberleninka.ru{l['href']}")
+        new_link = f"https://cyberleninka.ru{l['href']}"
+        if new_link not in links_list:
+            links_list.append(new_link)
     publish_year = soup.find("time")
     # remove ads
     ads = soup.find_all("div", {"class": "bibloid_serp bibloid_serp_ocr"})
     for ad in ads:
         ad.extract()
     article_text = soup.find("div", {"class": "ocr"})
-    article_file = open(f"articles/test_article_{iteration}_{publish_year.get_text()}.html", "w")
-    article_file.write(article_text.get_text())
+    article_file = open(f"articles/{iteration}_{publish_year.get_text()}.txt", "w", encoding='utf-8')
+    raw_text = article_text.get_text()
+    article_file.write(raw_text)
     article_file.close()
 
 
